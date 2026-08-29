c41334e057c1ba865b23603c5ecda5da177ed4b5
abbayosua <abbasiagian@gmail.com>
2026-08-29 09:17:17 +0700

    chore: gitignore uploads/ + config.php, update config.example.php

--- includes/config.example.php
+++ includes/config.example.php
@@ -11,4 +11,19 @@ define('SITE_NAME', 'TourAndTravel');
 
 // Session
 session_start();
+
+// Language switcher
+if (isset($_GET['lang']) && in_array($_GET['lang'], ['id', 'en'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+    setcookie('lang', $_GET['lang'], time() + (86400 * 365), '/');
+    $params = $_GET;
+    unset($params['lang']);
+    $redirect = strtok($_SERVER['REQUEST_URI'], '?');
+    if (!empty($params)) $redirect .= '?' . http_build_query($params);
+    header('Location: ' . $redirect);
+    exit;
+}
+if (!isset($_SESSION['lang']) && isset($_COOKIE['lang'])) {
+    $_SESSION['lang'] = $_COOKIE['lang'];
+}
 ?>
